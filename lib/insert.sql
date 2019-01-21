e8fb77a4b1be9e08e87fda5a446947036f51f542
Sid He <xhe518@gmail.com>
2019-01-21 23:58:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,31 +4,72 @@
 -- A pledge has an amount. It belongs to a user, and it also belongs to a project.
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("A", "a", "20", "1/21/2019", "1/22/2019");
+VALUES ("A", "a", 20, "1/21/2019", "1/22/2019");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("B", "b", "40", "1/22/2019", "1/23/2019");
+VALUES ("B", "b", 40, "1/22/2019", "1/23/2019");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("C", "c", "60", "1/23/2019", "1/24/2019");
+VALUES ("C", "c", 60, "1/23/2019", "1/24/2019");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("D", "d", "80", "1/24/2019", "1/25/2019");
+VALUES ("D", "d", 80, "1/24/2019", "1/25/2019");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("E", "e", "100", "1/25/2019", "1/26/2019");
+VALUES ("E", "e", 100, "1/25/2019", "1/26/2019");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("F", "f", "120", "1/21/2019", "1/22/2019");
+VALUES ("F", "f", 120, "1/21/2019", "1/22/2019");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("G", "g", "140", "1/22/2019", "1/23/2019");
+VALUES ("G", "g", 140, "1/22/2019", "1/23/2019");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("H", "h", "160", "1/23/2019", "1/24/2019");
+VALUES ("H", "h", 160, "1/23/2019", "1/24/2019");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("I", "i", "180", "1/24/2019", "1/25/2019");
+VALUES ("I", "i", 180, "1/24/2019", "1/25/2019");
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
-VALUES ("J", "j", "200", "1/25/2019", "1/26/2019");
+VALUES ("J", "j", 200, "1/25/2019", "1/26/2019");
+
+INSERT INTO users (name, age)
+VALUES ("AA", 20);
+INSERT INTO users (name, age)
+VALUES ("BB", 21);
+INSERT INTO users (name, age)
+VALUES ("CC", 22);
+INSERT INTO users (name, age)
+VALUES ("DD", 23);
+INSERT INTO users (name, age)
+VALUES ("EE", 24);
+INSERT INTO users (name, age)
+VALUES ("FF", 25);
+INSERT INTO users (name, age)
+VALUES ("GG", 26);
+INSERT INTO users (name, age)
+VALUES ("HH", 27);
+INSERT INTO users (name, age)
+VALUES ("II", 28);
+INSERT INTO users (name, age)
+VALUES ("JJ", 29);
+INSERT INTO users (name, age)
+VALUES ("KK", 30);
+INSERT INTO users (name, age)
+VALUES ("LL", 31);
+INSERT INTO users (name, age)
+VALUES ("MM", 32);
+INSERT INTO users (name, age)
+VALUES ("NN", 33);
+INSERT INTO users (name, age)
+VALUES ("OO", 34);
+INSERT INTO users (name, age)
+VALUES ("PP", 35);
+INSERT INTO users (name, age)
+VALUES ("QQ", 36);
+INSERT INTO users (name, age)
+VALUES ("RR", 37);
+INSERT INTO users (name, age)
+VALUES ("SS", 38);
+INSERT INTO users (name, age)
+VALUES ("TT", 39);
