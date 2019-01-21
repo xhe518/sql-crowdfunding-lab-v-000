896872eb9bcc2c456d163dc28f4e5d6b5fb8a6ef
Sid He <xhe518@gmail.com>
2019-01-21 23:53:23 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
-# Within the insert.sql file, insert 10 projects, 20 users, and 30 pledges into the database
-# A project has a title, a category, a funding goal, a start date, and an end date.
-# A user has a name and an age
-# A pledge has an amount. It belongs to a user, and it also belongs to a project.
+-- Within the insert.sql file, insert 10 projects, 20 users, and 30 pledges into the database
+-- A project has a title, a category, a funding goal, a start date, and an end date.
+-- A user has a name and an age
+-- A pledge has an amount. It belongs to a user, and it also belongs to a project.
 
 INSERT INTO projects (title, category, funding_goal, start_date, end_date)
 VALUES ("A", "a", "20", "1/21/2019", "1/22/2019");
