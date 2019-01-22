d40d26557315a1095a052b6d3e20f35860a99917
Sid He <xhe518@gmail.com>
2019-01-22 00:26:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,6 +18,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 "SELECT users.name, users.age, pledges.amount
 FROM users
 JOIN pledges ON users.id = pledges.user_id
+GROUP BY users.name
 ORDER BY users.name"
 end
 
