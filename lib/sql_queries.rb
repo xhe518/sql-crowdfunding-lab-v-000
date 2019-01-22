a8d61114ee99cef3923c39627c20a56f12c7ed3f
Sid He <xhe518@gmail.com>
2019-01-22 00:53:24 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,7 +33,7 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "SELECT name, SUM(pledges.amount)
 FROM users
-INNER JOIN pledges ON users.id = pledges.user_id
+JOIN pledges ON users.id = pledges.user_id
 GROUP BY users.name
 ORDER BY SUM(pledges.amount), users.name;"
 end
@@ -46,5 +46,8 @@ WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT category, SUM(pledges.amount)
+FROM projects
+JOIN pledges ON projects.id = pledges.project_id
+WHERE projects.category = 'books';"
 end
