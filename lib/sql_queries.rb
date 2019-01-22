d0bcf2afc78b0cc7a44c8e35e3fbb15f6b8b603a
Sid He <xhe518@gmail.com>
2019-01-22 00:18:21 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -12,7 +12,7 @@ FROM projects
 LEFT JOIN pledges ON projects.id = pledges.project_id
 LEFT JOIN users ON pledges.user_id = users.id
 GROUP BY projects.id
-ORDER BY users.name"
+ORDER BY users.name;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
