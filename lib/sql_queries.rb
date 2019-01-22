efe47b995489e3ae2cec9b417b5580306feb8fab
Sid He <xhe518@gmail.com>
2019-01-22 00:38:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.titles, SUM(pledges.amount)-projects.funding_goal
+"SELECT projects.title, SUM(pledges.amount)-projects.funding_goal
 FROM projects
 JOIN pledges ON projects.id = pledges.project_id
 GROUP BY projects.title
