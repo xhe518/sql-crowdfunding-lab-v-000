a46e1c13f37db201ded600c42a2304b6b19879c5
Sid He <xhe518@gmail.com>
2019-01-22 00:37:42 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,15 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 FROM users
 JOIN pledges ON users.id = pledges.user_id
 GROUP BY users.name
-ORDER BY users.name"
+ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.titles, SUM(pledges.amount)-projects.funding_goal
+FROM projects
+JOIN pledges ON projects.id = pledges.project_id
+GROUP BY projects.title
+HAVING SUM(pledges.amount) > projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
